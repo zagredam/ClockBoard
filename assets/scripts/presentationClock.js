f4ebc6016492f329a9d10fa3ccf1c9272ed6bce8
Michael Zagreda <zagredam@gmail.com>
2024-04-11 21:07:14 -0400

    added toggles

--- assets/scripts/presentationClock.js
+++ assets/scripts/presentationClock.js
@@ -22,6 +22,22 @@ Month[11] = {Full: "December",Abbr:"Dec"};
 var schedule = [];//[{Description:"Lunch",Time:new Date("Sat Sep 14 2020 11:54:41 GMT-0400 (Eastern Daylight Time)")},
   //escription:"Wedding",Time:new Date("Sat Sep 14 2020 14:30:00 GMT-0400 (Eastern Daylight Time)")}];
 var currentTime = new Date();
+SHOWSECONDS = true;
+SHOWMERIDUM = true;
+SHOWDATE = true;
+function toggleDate(){
+  SHOWDATE = !SHOWDATE;
+  document.getElementById("dateContent").style.display = !SHOWDATE ? "none":"unset";
+}
+function toggleMeridum(){
+  SHOWMERIDUM = !SHOWMERIDUM;
+  document.getElementById("clockMeridium").style.display = !SHOWMERIDUM ? "none":"unset";
+}
+function toggleSeconds(){
+  SHOWSECONDS = !SHOWSECONDS;
+  document.getElementById("secondsSpan").style.display = !SHOWSECONDS ? "none":"unset";
+  
+}
 function setTime(){
     var d = new Date();
     var n = d.getTime();
@@ -30,10 +46,17 @@ function setTime(){
     var seconds = d.getSeconds();
     document.getElementById("clockMeridium").innerText = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
-    document.getElementById("clockHour").innerHTML =  (hours ? hours : 12) ; // the hour '0' should be '12'
-    document.getElementById("clockMinute").innerHTML = (minutes < 10 ? '0'+minutes : minutes) 
-    document.getElementById("clockSecond").innerHTML = (seconds < 10 ? '0'+seconds : seconds) ;
+    document.getElementById("clockHourDigit1").innerText =  (hours == 0 || hours >= 10 ? "1"  : "") ; // the hour '0' should be '12'
+    document.getElementById("clockHourDigit1").style.display = (hours == 0 || hours >= 10 ? "unset"  : "none") ;
+    document.getElementById("clockHourDigit2").innerText =  (hours ? hours  % 10 : 2) ; // the hour '0' should be '12'
+    document.getElementById("clockMinuteDigit1").innerText = Math.floor(minutes / 10);
+    document.getElementById("clockMinuteDigit2").innerText = minutes % 10;
+    if(SHOWSECONDS){
+      document.getElementById("clockSecondDigit1").innerText = Math.floor(seconds / 10);
+    document.getElementById("clockSecondDigit2").innerText = seconds % 10;
+    //document.getElementById("clockSecond").innerHTML = (seconds < 10 ? '0'+seconds : seconds) ;
     // document.getElementById("timevalue").innerText=`${(hours ? hours : 12)} :${(minutes < 10 ? '0'+minutes : minutes) } : ${("clockSecond").innerHTML = (seconds < 10 ? '0'+seconds : seconds) } ${hours >= 12 ? 'pm' : 'am'}`;
+    }
     if(d.getDay() != currentTime.getDay()){
       currentTime = new Date();
       document.getElementById("dateContent").innerText = WeekDay[currentTime.getDay()].Full+", "+Month[currentTime.getMonth()].Full+ " "+currentTime.getDate() +", "+currentTime.getFullYear();
@@ -72,3 +95,12 @@ function updateNotifications(){
   }
   startPresentationTimer();
   document.getElementById("dateContent").innerText = WeekDay[currentTime.getDay()].Full+", "+Month[currentTime.getMonth()].Full+ " "+currentTime.getDate()+", "+currentTime.getFullYear();
+  
+  HIDENAVTIMEOUT = null;
+  document.addEventListener("mousemove",()=>{
+    if(HIDENAVTIMEOUT != null) clearTimeout(HIDENAVTIMEOUT);
+    document.getElementById("mainNavIcon").classList.add("show");
+    HIDENAVTIMEOUT = setTimeout(()=>{
+      document.getElementById("mainNavIcon").classList.remove("show");
+    },2000)
+  })
